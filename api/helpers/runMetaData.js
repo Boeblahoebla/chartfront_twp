36cc3ad0a5bb3a7deeaa4d343ebd692c9e68e036
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2025-03-15 21:32:17 +0100

    removed some comments

--- api/helpers/runMetaData.js
+++ api/helpers/runMetaData.js
@@ -8,9 +8,9 @@ export const getGfsRunMetadata = async () => {
     try {
         const jsonPath = path.resolve("../src/images/gfs_run_metadata.json");
         const data = await fs.readFile(jsonPath, "utf8");
-        return JSON.parse(data); // Convert string to JSON
+        return JSON.parse(data);
     } catch (error) {
         console.error("Error reading JSON file:", error);
-        return {}; // Return empty object on failure
+        return {};
     }
 };
